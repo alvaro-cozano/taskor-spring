ebbab96a80e279fec34c5d3f0f48bf90ba76e0ad
alvaro-cozano <alvarocozano@gmail.com>
2025-05-27 09:56:28 +0200

    Modificaciones para despliegue

--- src/main/java/com/project/management/springboot/backend/project_management/websockets/config/WebSocketConfig.java
+++ src/main/java/com/project/management/springboot/backend/project_management/websockets/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.project.management.springboot.backend.project_management.websockets.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -11,19 +10,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${app.front-url}")
-    private String frontendUrl;
+        @Override
+        public void configureMessageBroker(MessageBrokerRegistry config) {
+                config.enableSimpleBroker("/topic");
+                config.setApplicationDestinationPrefixes("/app");
+        }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns(frontendUrl)
-                .withSockJS();
-    }
+        @Override
+        public void registerStompEndpoints(StompEndpointRegistry registry) {
+                registry.addEndpoint("/ws")
+                                .setAllowedOriginPatterns("*")
+                                .withSockJS();
+        }
 }
